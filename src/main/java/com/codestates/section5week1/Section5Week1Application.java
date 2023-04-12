b81ccc4d8cf8c21a19cc4359a86aab156066e69f
donghee <jjj4435@naver.com>
2023-04-12 16:00:23 +0900

    main

---
+++ src/main/java/com/codestates/section5week1/Section5Week1Application.java
@@ -0,0 +1,13 @@
+package com.codestates.section5week1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section5Week1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section5Week1Application.class, args);
+	}
+
+}
